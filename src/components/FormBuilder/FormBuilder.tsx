127d431ab44b38701fae4d4d07a2de75f6cb64b3
ThangaDhiwanV <vthangadhiwan02@gmail.com>
2025-07-15 18:47:59 +0530

    Fix JSX syntax error in FormBuilder

--- src/components/FormBuilder/FormBuilder.tsx
+++ src/components/FormBuilder/FormBuilder.tsx
@@ -882,6 +882,9 @@ const FormBuilder: React.FC = () => {
             </>
           )}
         </div>
+
+        {/* Delete Confirmation Modal */}
+        {deleteModalOpen && itemToDelete && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white rounded-lg shadow-xl max-w-md w-full mx-4">
               <div className="p-6">
