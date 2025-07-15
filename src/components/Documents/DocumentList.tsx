eb9cb5738864aa5be55feaff092f4c75e3a1d4ef
ThangaDhiwanV <vthangadhiwan02@gmail.com>
2025-07-15 11:42:12 +0530

    Move clear button to last position in Documents page

--- src/components/Documents/DocumentList.tsx
+++ src/components/Documents/DocumentList.tsx
@@ -401,23 +401,6 @@ const DocumentList: React.FC = () => {
                 <option value="Name">Name</option>
                 <option value="Type">Type</option>
                 <option value="Version">Version</option>
-                <option value="Status">Status</option>
-                <option value="Created By">Created By</option>
-                <option value="Assigned To">Assigned To</option>
-                <option value="Created Date">Created Date</option>
-                <option value="Due Date">Due Date</option>
-              </select>
-              <button
-                onClick={() => setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')}
-                className="p-1 hover:bg-gray-100 rounded transition-colors"
-              >
-                {sortDirection === 'asc' ? 
-                  <ChevronUp className="w-4 h-4 text-gray-500" /> : 
-                  <ChevronDown className="w-4 h-4 text-gray-500" />
-                }
-              </button>
-            </div>
-
             {/* Created Date Filter */}
             <div className="flex items-center space-x-2">
               <Calendar className="w-4 h-4 text-gray-500" />
@@ -434,6 +417,23 @@ const DocumentList: React.FC = () => {
                 <option value="Last 30 Days">Last 30 Days</option>
               </select>
             </div>
+
+            {/* Clear Filters */}
+            <button
+              onClick={clearFilters}
+              className={`px-3 py-2 border rounded-lg transition-colors text-sm relative ${
+                getActiveFiltersCount() > 0 
+                  ? 'bg-blue-50 border-blue-300 text-blue-700 hover:bg-blue-100' 
+                  : 'text-gray-600 border-gray-300 hover:bg-gray-50'
+              }`}
+            >
+              Clear
+              {getActiveFiltersCount() > 0 && (
+                <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                  {getActiveFiltersCount()}
+                </span>
+              )}
+            </button>
 
             {/* Clear Filters */}
             <button
